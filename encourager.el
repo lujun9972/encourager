c845d35a58f4bb3402a8f87728e78885e704e68d
darksun <lujun9972@sina.com>
2016-02-22 14:31:38 +0800

    add package header

--- encourager.el
+++ encourager.el
@@ -1,13 +1,48 @@
-;; -*- lexical-binding: t; -*-
+;;; encourager.el --- atom-miku like plugin  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2004-2015 Free Software Foundation, Inc.
+
+;; Author: DarkSun <lujun9972@gmail.com>
+;; Created: 2016-01-06
+;; Version: 0.1
+;; Keywords: convenience, image
+;; URL: https://github.com/lujun9972/encourager
+
+;; This file is NOT part of GNU Emacs.
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Source code
+;;
+;; encourager's code can be found here:
+;;   http://github.com/lujun9972/encourager
+
+;;; Commentary:
+
+;; encourager is an atom-miku like plugin
+
+;;; Code:
 
 (defgroup encourager nil
   "")
 
 (defcustom encourager-buffer "*encourager"
   " Buffer used to display encourager-image"
-  :group 'encourager)
+  :group 'encourager
+  :type 'string)
 
-(defcustom encourager-image-file (concat (file-name-directory buffer-file-name)  "dancing.gif")
+(defcustom encourager-image-file (concat (file-name-directory buffer-file-name)  "miku.gif")
   "Multi-frame image file to be displayed as encourager"
   :group 'encourager
   :type '(file :must-match t))
